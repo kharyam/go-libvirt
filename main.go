8174a81215e260ab65c8e3181d34418484927095
Khary Mendez <kharyam@gmail.com>
2025-05-02 18:51:45 -0400

    main.go

--- main.go
+++ main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
+	"strconv"
+	"time"
 
 	libvirt "github.com/libvirt/libvirt-go"
 )
+
+const nanosecondsPerSecond = 1e9 // 1 billion nanoseconds in a second
 
 func main() {
 	// Connect to the local libvirt daemon
@@ -21,45 +26,127 @@ func main() {
 		log.Fatalf("Failed to list domains: %v", err)
 	}
 
-	fmt.Printf("Running VMs: %d\n", len(domainIDs))
+	// Configure sleep duration (default to 500ms)
+	sleepDuration := 500 * time.Millisecond
 
-	for _, id := range domainIDs {
-		dom, err := conn.LookupDomainById(id)
-		if err != nil {
-			log.Printf("Failed to lookup domain by ID %d: %v", id, err)
-			continue
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		fmt.Fprintf(w, "<h1>Libvirt VM Statistics</h1>")
+		fmt.Fprintf(w, "<table border='1'><tr><th>VM Name</th><th>State</th><th>Used Memory</th><th>VCPUs</th><th>CPU Time</th><th>Unused Memory (GB)</th><th>Max Memory (GB)</th></tr>")
+
+		// Capture initial CPU times
+		initialCPUTimes := make(map[uint32]float64)
+		for _, id := range domainIDs {
+			dom, err := conn.LookupDomainById(id)
+			if err != nil {
+				log.Printf("Failed to lookup domain by ID %d: %v", id, err)
+				continue
+			}
+			defer dom.Free()
+
+			info, err := dom.GetInfo()
+			if err != nil {
+				name, err := dom.GetName()
+				if err != nil {
+					log.Printf("Failed to get info or name for domain ID %d: %v", id, err)
+				} else {
+					log.Printf("Failed to get info for domain %s: %v", name, err)
+				}
+				continue
+			}
+
+			initialCPUTimes[id] = float64(info.CpuTime)
 		}
-		defer dom.Free()
 
-		name, _ := dom.GetName()
-		info, err := dom.GetInfo()
-		if err != nil {
-			log.Printf("Failed to get info for domain %s: %v", name, err)
-			continue
+		// Wait for the specified duration
+		time.Sleep(sleepDuration)
+
+		// Capture final CPU times
+		finalCPUTimes := make(map[uint32]float64)
+		for _, id := range domainIDs {
+			dom, err := conn.LookupDomainById(id)
+			if err != nil {
+				log.Printf("Failed to lookup domain by ID %d: %v", id, err)
+				continue
+			}
+			defer dom.Free()
+
+			info, err := dom.GetInfo()
+			if err != nil {
+				name, err := dom.GetName()
+				if err != nil {
+					log.Printf("Failed to get info or name for domain ID %d: %v", id, err)
+				} else {
+					log.Printf("Failed to get info for domain %s: %v", name, err)
+				}
+				continue
+			}
+
+			finalCPUTimes[id] = float64(info.CpuTime)
 		}
 
-		fmt.Printf("\nVM Name: %s\n", name)
-		fmt.Printf("  State: %v\n", info.State)
-		fmt.Printf("  Max Memory: %d KB\n", info.MaxMem)
-		fmt.Printf("  Used Memory: %d KB\n", info.Memory)
-		fmt.Printf("  Number of VCPUs: %d\n", info.NrVirtCpu)
-		fmt.Printf("  CPU Time: %d ns\n", info.CpuTime)
+		// Calculate CPU usage percentages
+		for _, id := range domainIDs {
+			dom, err := conn.LookupDomainById(id)
+			if err != nil {
+				log.Printf("Failed to lookup domain by ID %d: %v", id, err)
+				continue
+			}
 
+			info, err := dom.GetInfo()
+			if err != nil {
+				name, err := dom.GetName()
+				if err != nil {
+					log.Printf("Failed to get info or name for domain ID %d: %v", id, err)
+				} else {
+					log.Printf("Failed to get info for domain %s: %v", name, err)
+				}
+				continue
+			}
 
-    memStats, err := dom.MemoryStats(11, 0)
-    if err != nil {
-    	log.Printf("Failed to get memory stats for domain %s: %v", name, err)
-    } else {
-    	for _, stat := range memStats {
-    		switch libvirt.DomainMemoryStatTags(stat.Tag) {
-    		case libvirt.DOMAIN_MEMORY_STAT_ACTUAL_BALLOON:
-    			fmt.Printf("  Actual Memory (balloon): %d KB\n", stat.Val)
-    		case libvirt.DOMAIN_MEMORY_STAT_RSS:
-    			fmt.Printf("  Resident Set Size (RSS): %d KB\n", stat.Val)
-    		case libvirt.DOMAIN_MEMORY_STAT_UNUSED:
-    			fmt.Printf("  Unused Memory: %d KB\n", stat.Val)
-    		}
-    	}
-    }
-	}
+			cpuTimeDifference := finalCPUTimes[id] - initialCPUTimes[id]
+			numVCPUs := float64(info.NrVirtCpu)
+			cpuUsagePercentage := (cpuTimeDifference / (numVCPUs * nanosecondsPerSecond)) * 100
+
+			// Get memory stats (for display)
+			var unusedMemory int
+			memStats, err := dom.MemoryStats(11, 0)
+			if err != nil {
+				name, err := dom.GetName()
+				if err != nil {
+					log.Printf("Failed to get memory stats for domain ID %d: %v", id, err)
+				} else {
+					log.Printf("Failed to get memory stats for domain %s: %v", name, err)
+				}
+			} else {
+				for _, stat := range memStats {
+					switch libvirt.DomainMemoryStatTags(stat.Tag) {
+					case libvirt.DOMAIN_MEMORY_STAT_UNUSED:
+						unusedMemory = int(stat.Val)
+					}
+				}
+			}
+
+			name, err := dom.GetName()
+			if err != nil {
+				log.Printf("Failed to get name for domain ID %d: %v", id, err)
+				name = "Unknown"
+			}
+
+			memoryUsedPercentage := 0.0
+			if info.MaxMem > 0 {
+				memoryUsedPercentage = float64(info.MaxMem-uint64(unusedMemory)) / float64(info.MaxMem) * 100
+			}
+
+			maxMemoryGB := float64(info.MaxMem) / (1024 * 1024)
+			unusedMemoryGB := float64(unusedMemory) / (1024 * 1024)
+			fmt.Fprintf(w, "<tr><td>%s</td><td>%v</td><td>%.2f%%</td><td>%d</td><td>%.2f%%</td><td>%.2f</td><td>%.2f</td></tr>", name, info.State, memoryUsedPercentage, info.NrVirtCpu, cpuUsagePercentage, unusedMemoryGB, maxMemoryGB)
+
+		}
+		fmt.Fprintf(w, "</table>")
+	})
+
+	port := 8080
+	log.Printf("Server listening on port %d", port)
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), nil))
 }
